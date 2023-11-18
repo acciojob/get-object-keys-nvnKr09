cafce7a190b732218ff259192952f0290d7ee528
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-11-18 20:52:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const student = {
+  name: "John"
+};
+
+// Add a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
